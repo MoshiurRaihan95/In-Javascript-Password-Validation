35d6858e9d31f1a696929ffed07eff04ebb74ddd
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2022-09-16 18:38:49 +0600

    Password show hide

--- pasword.js
+++ pasword.js
@@ -2,3 +2,11 @@ let paswrd = document.getElementById('paswrd');
 let togglebtn = document.getElementById('togglebtn');
 
 // Show Hide Password
+togglebtn.onclick = function(){
+    if(paswrd.type === 'password'){
+        paswrd.setAttribute('type', 'text');
+    }
+    else{
+        paswrd.setAttribute('type', 'password');
+    }
+}
