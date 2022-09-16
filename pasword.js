b384665d51cad844b7427339e3a3ab61062822bb
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2022-09-16 19:19:04 +0600

    reguler expresion done

--- pasword.js
+++ pasword.js
@@ -1,5 +1,24 @@
 let paswrd = document.getElementById('paswrd');
 let togglebtn = document.getElementById('togglebtn');
+let lowercase = document.getElementById('lower');
+let uppercase = document.getElementById('upper');
+let digit = document.getElementById('number');
+let specialchar = document.getElementById('special');
+let totalpass = document.getElementById('length');
+
+
+// Match Password
+function checkPassword(data){
+    // javaScript regular expresion parttern
+    const lower = new RegExp('(?=.*[a-z])');
+    const upper = new RegExp('(?=.*[a-Z])');
+    const number = new RegExp('(?=.*[0-9])');
+    const special = new RegExp('(?=.*[!@#\$%\^&\*])');
+    const length = new RegExp('(?=.{8,})');
+
+    // Lowercase Validation check
+    
+}
 
 // Show Hide Password
 togglebtn.onclick = function(){
