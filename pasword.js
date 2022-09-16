172dc16dfad2ac3baa3a5aa33f128a3dd973092e
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2022-09-16 19:59:38 +0600

    validation check done

--- pasword.js
+++ pasword.js
@@ -1,7 +1,7 @@
 let paswrd = document.getElementById('paswrd');
 let togglebtn = document.getElementById('togglebtn');
-let lowercase = document.getElementById('lower');
-let uppercase = document.getElementById('upper');
+let lowerCase = document.getElementById('lower');
+let upperCase = document.getElementById('upper');
 let digit = document.getElementById('number');
 let specialchar = document.getElementById('special');
 let totalpass = document.getElementById('length');
@@ -11,13 +11,46 @@ let totalpass = document.getElementById('length');
 function checkPassword(data){
     // javaScript regular expresion parttern
     const lower = new RegExp('(?=.*[a-z])');
-    const upper = new RegExp('(?=.*[a-Z])');
+    const upper = new RegExp('(?=.*[A-Z])');
     const number = new RegExp('(?=.*[0-9])');
     const special = new RegExp('(?=.*[!@#\$%\^&\*])');
     const length = new RegExp('(?=.{8,})');
 
     // Lowercase Validation check
-    
+    if(lower.test(data)){
+        lowerCase.classList.add('valid');
+    }
+    else{
+        lowerCase.classList.remove('valid');
+    }
+    // Uppercase Validation check
+    if(upper.test(data)){
+        upperCase.classList.add('valid');
+    }
+    else{
+        upperCase.classList.remove('valid');
+    }
+    // Number Validation check
+    if(number.test(data)){
+        digit.classList.add('valid');
+    }
+    else{
+        digit.classList.remove('valid');
+    }
+    // Special Charecter Validation check
+    if(special.test(data)){
+        specialchar.classList.add('valid');
+    }
+    else{
+        specialchar.classList.remove('valid');
+    }
+    // Minimu * Charecter password Validation check
+    if(length.test(data)){
+        totalpass.classList.add('valid');
+    }
+    else{
+        totalpass.classList.remove('valid');
+    }
 }
 
 // Show Hide Password
